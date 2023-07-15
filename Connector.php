ccd9e2459341a31fe2b7cc3e7a3f0e0a6773f901
bipen-acharya <85977459+bipen-acharya@users.noreply.github.com>
2023-07-15 12:47:26 +0545

    first commit

---
+++ Connector.php
@@ -0,0 +1,22 @@
+<?php
+
+$servername = "192.168.2.101";
+$username = "root";
+$password = "";
+$dbname = "ritin";
+$table = "users";
+// lets create a table named Employees.
+// we will get actions from the app to do operations in the database...
+
+// Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$action = isset($_POST["action"]) ? $_POST["action"] : ""; // lets get the action
+
+// Check Connection
+if ($conn->connect_errno) {
+    die("Connection Failed: " . $conn->connect_error);
+    echo 'action: ' . $action;
+    return;
+} else {
+    //echo ("Sucess");
+}
